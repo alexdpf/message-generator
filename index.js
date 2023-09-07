e2ee8b0806ede7a32cdc261fce5ec5c61fd8a117
Alex Fernandes <43734868+alexdpf@users.noreply.github.com>
2023-09-07 16:40:59 +1000

    Create finalQuote function to swap (technology) string inside  property value for  property value

--- index.js
+++ index.js
@@ -7,37 +7,41 @@ const {quoteDatabase} = require('./data.js');
 
 //4. create a function to select a random `author`
 const randomAuthor = (database) => {
-    let pickRandomIndex = Math.floor(Math.random() * 29);
+    let pickRandomIndex = Math.floor(Math.random() * database.quotes.length);
     return database.quotes[pickRandomIndex].author;
 }
 
-// function randomAuthor test
-console.log(randomAuthor(quoteDatabase));
+// TEST function randomAuthor
+//console.log(randomAuthor(quoteDatabase));
 
 //5. create a function to select a random `technology`
 const randomTech = (database) => {
-    let pickRandomIndex = Math.floor(Math.random() * 29);
+    let pickRandomIndex = Math.floor(Math.random() * database.quotes.length);
     return database.quotes[pickRandomIndex].technology;
 }
 
 
-//function randomTech test
-console.log(randomTech(quoteDatabase));
+//TEST function randomTech
+//console.log(randomTech(quoteDatabase));
 
 //6. create a function to select a random `technology`
 const randomQuote = (database) => {
-    let pickRandomIndex = Math.floor(Math.random() * 29);
+    let pickRandomIndex = Math.floor(Math.random() * database.quotes.length);
     return database.quotes[pickRandomIndex].quote;
 }
 
-//function randomQuote test
-console.log(randomQuote(quoteDatabase));
+//TEST function randomQuote
+//console.log(randomQuote(quoteDatabase));
 
-//7. function to prints message combining three different pieces of data.
-const messageGenerator = (database) => {
+//8. create function to swap (technology) string inside `quote` property value for `technology` property value
+//pick one random quote
+const swapTechValue = randomQuote(quoteDatabase)
 
-    console.log(`${randomAuthor(quoteDatabase)} + ${randomTech(quoteDatabase)} + ${randomQuote(quoteDatabase)}`);
-   // console.log(randomAuthor + randomTech + randomQuote);
-}
+//swap placeholder (technology) for a random object property `technology`
+const finalQuote = swapTechValue.replace('(technology)', `${randomTech(quoteDatabase)}`);
 
-messageGenerator(quoteDatabase)
+//TEST function finalQuote
+console.log(`${finalQuote}.
+${randomAuthor(quoteDatabase)}`);
+
+
