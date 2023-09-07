7689c0cd00351b93961c0c9ecec175ca2a0acef2
Alex Fernandes <43734868+alexdpf@users.noreply.github.com>
2023-09-07 11:00:10 +1000

    Create randomAuthor function, debugging process to begin

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-//File to write functions that will import object properties from data.js and randomly outputs a message combining three pieces of data being: {author}, {quote}, and {technology}
+//File to write functions that will import object properties from data.js
+//Randomly outputs a message combining three pieces of data being: {author}, {quote}, and {technology}
 
+//import object property values from data.js
+import {quotes: {author}} from './data.js';
+
+//create a function to select a random `author`
+const randomAuthor = () => {
+    let pickRandom = author[index](MathfloorMath.floor(Math.random() * 30));
+}
+
+//function test - SyntaxError: Cannot use import statement outside a module
+console.log(randomAuthor);
+
+//use it later
+//const messageGenerator = (author, quote, technology) => {
+//    let randomMessage = ''
+//}
